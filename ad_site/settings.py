ca0806a9a6c58e2dca3edf6271f2451d12da9639
Anton Knyazev <knyazevmac@icloud.com>
2024-12-12 04:20:30 +0400

    sdkjvndss

--- ad_site/settings.py
+++ ad_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4*d@@eyq&!3cglu5=or@!trd(zy-&qvvt2z7$#zf2u0f=2phap'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
